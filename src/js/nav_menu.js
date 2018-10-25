b6d4d2b735f9dbd79981d7f3a6c0a099e17b1810
dpinchuk <pinchuk.dap@gmail.com>
2018-10-25 22:52:24 +0300

    in progress...

--- src/js/nav_menu.js
+++ src/js/nav_menu.js
@@ -1,5 +1,10 @@
-document.addEventListener('click', function(e){
-    if (e.target.closest('.header__navbar-nav-button')) {
-        document.getElementsByClassName('header__navbar-nav')[0].classList.toggle('active')
-    }
+// document.addEventListener('click', function(e){
+//     if (e.target.closest('.header__navbar-nav-button')) {
+//         document.getElementsByClassName('header__navbar-nav')[0].classList.toggle('active')
+//     }
+// });
+
+jQuery(document).ready(function($){
+    $('.header__navbar-nav-button').show();
+    $('.header__navbar-nav-menu').click(function(){$(this).next().toggle()});
 });
