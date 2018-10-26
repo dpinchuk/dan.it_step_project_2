0bc92c2a6ef94056818ce169c56eaab0cc60e066
dpinchuk <pinchuk.dap@gmail.com>
2018-10-26 16:57:56 +0300

    in progress

--- src/js/section_social.js
+++ src/js/section_social.js
@@ -12,15 +12,15 @@ let arrayObjectSocial = [
         name: "ivan ionov",
         position: "@EngineerD",
         span: "@Sizebuddha",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur eligendi eos est excepturi exercitationem maiores",
+        description: "Ab ad architecto aspernatur commodi consectetur culpa, dolorum et expedita inventore, dolorum et expedita inventore, dixi",
         time: "3 hours ago"
     },
     {
         img: "img/common/section-social/social-img-3.png",
-        name: "alex guetta",
+        name: "david guetta",
         position: "@SupportX",
         span: "@Redisbuddha",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur eligendi eos est excepturi exercitationem maiores",
+        description: "Adipisci aperiam cum, doloribus impedit non perferendis placeat provident quam sint suscipit, doloribus impedit non perferendis",
         time: "4 hours ago"
     },
     {
@@ -28,7 +28,7 @@ let arrayObjectSocial = [
         name: "erich fildman",
         position: "@DeveloperJ",
         span: "@Writebuddha",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur eligendi eos est excepturi exercitationem maiores",
+        description: "Assumenda consequuntur cumque doloremque eum, magni, minus molestiae quibusdam reiciendis repellat, repudiandae similique ut",
         time: "5 hours ago"
     },
 ];
@@ -45,19 +45,19 @@ let readySocial = () =>  {
         "<img src='" + arrayObjectSocial[0].img + "' alt='' class='section-social__carusel-social-item__img'>" +
         "<h3 class='section-social__carusel-social-item__name text-capitalize'>" + arrayObjectSocial[0].name + "</h3>" +
         "<h3 class='section-social__carusel-social-item__position text-capitalize'>" + arrayObjectSocial[0].position + "</h3>" +
-        "<p class='section-social__carusel-social-item__description'><span>" + arrayObjectSocial[0].span + "</span></p>" +
+        "<p class='section-social__carusel-social-item__description'><span>" + arrayObjectSocial[0].span + "</span>" + " " + arrayObjectSocial[0].description + "</p>" +
         "<h3 class='section-social__carusel-social-item__time'>" + arrayObjectSocial[0].time + "</h3>";
 };
 
 document.addEventListener("DOMContentLoaded", readySocial);
 
 let showImages = () => {
-    clearDiv("socialItem");
+    //clearDiv("socialItem");
     document.getElementById("socialItem").innerHTML =
         "<img src='" + arrayObjectSocial[current].img + "' alt='' class='section-social__carusel-social-item__img'>" +
         "<h3 class='section-social__carusel-social-item__name text-capitalize'>" + arrayObjectSocial[current].name + "</h3>" +
         "<h3 class='section-social__carusel-social-item__position text-capitalize'>" + arrayObjectSocial[current].position + "</h3>" +
-        "<p class='section-social__carusel-social-item__description'><span>" + arrayObjectSocial[current].span + "</span></p>" +
+        "<p class='section-social__carusel-social-item__description'><span>" + arrayObjectSocial[current].span + "</span>" + " " + arrayObjectSocial[current].description + "</p>" +
         "<h3 class='section-social__carusel-social-item__time'>" + arrayObjectSocial[current].time + "</h3>";
 
 };
@@ -69,7 +69,7 @@ let previous = () => {
     }
     let temp = arrayCurrent.pop();
     arrayCurrent.unshift(temp);
-    showImages(arrayAboutImages, current);
+    showImages(arrayCurrent, current);
 };
 
 let next = () => {
@@ -78,6 +78,6 @@ let next = () => {
         current = 0;
     }
     let temp = arrayCurrent.shift();
-    arrayCurrent[arrayAboutImages.length] = temp;
-    showImages(arrayAboutImages, current);
+    arrayCurrent[arrayCurrent.length] = temp;
+    showImages(arrayCurrent, current);
 };
