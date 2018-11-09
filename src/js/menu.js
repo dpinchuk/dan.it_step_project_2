924e0eb3f00afc49708063f1290128b33314bbe5
dpinchuk <pinchuk.dap@gmail.com>
2018-11-09 07:07:51 +0200

    added new sections

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,12 @@
-(function($){
-    $(function() {
-        $('.btn__menu').on('click', function() {
-            $(this).closest('.menu').toggleClass('menu-open');
-        });
-    });
-})(jQuery);
+// (function($){
+//     $(function() {
+//         $('.btn__menu').on('click', function() {
+//             $(this).closest('.menu').toggleClass('menu-open');
+//         });
+//     });
+// })(jQuery);
+
+// jQuery(document).ready(function($){
+//     $('.menu').hide();
+//     $('.btn__menu').click(function(){$(this).next().toggle()});
+// });
