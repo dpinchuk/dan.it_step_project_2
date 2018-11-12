aed01be7d513057b52f2696dbc6a6b39a1db63ee
dpinchuk <pinchuk.dap@gmail.com>
2018-11-12 08:02:16 +0200

    added new sections

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,4 @@
 let showMenu = () => {
-
+    document.getElementsByClassName('btn__menu')[0].style.backgroundImage = "url('../img/320/header/cross.png') no-repeat";
+    // document.getElementsByClassName('menu')[0].style.display = "block";
 };
