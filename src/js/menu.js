33cf6ecec3c1fbc9947c1629e4cc2915a05a5163
dpinchuk <pinchuk.dap@gmail.com>
2018-11-12 23:42:13 +0200

    test

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,4 +1,20 @@
 let showMenu = () => {
-    document.getElementsByClassName('btn__menu')[0].style.backgroundImage = "url('../img/320/header/cross.png') no-repeat";
-    // document.getElementsByClassName('menu')[0].style.display = "block";
+    let divButton = document.getElementById("fokioMenu");
+
+    let ddd = document.getElementsByClassName("btn__menu");
+
+    let divMenu = document.getElementById("menuItems");
+
+    if (divMenu.style.display === 'block') {
+        divButton.style.backgroundImage = 'url(../img/common/header/header_button_menu.png) no-repeat;';
+        divMenu.style.display = 'none';
+    } else {
+        //ddd[0]. = "";
+        //ddd[0].style = 'background: url(../img/common/header/cross.png) no-repeat;';
+        //ddd[0].style.width = "50px";
+        ddd.classList.add('cross');
+
+        divMenu.style.display = 'block';
+    }
+
 };
